2a909cc4b9a2e319fab3556039d44889b18d6950
PColares <siqueira.pedro1998@gmail.com>
2024-07-08 11:23:08 -0300

    pedro/add: implementação de login e registro de usuários

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
-export  default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",   
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-right": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(100%, 0, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        fadeinright: "fade-in-right 1s ease-in-out 0.25s 1",
+      },
+    },
   },
-  plugins: [
-  
-  ],
-}
-
+  plugins: [],
+};
