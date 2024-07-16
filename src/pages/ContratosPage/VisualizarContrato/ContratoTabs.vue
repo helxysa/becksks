2ceca729ca08d17da0347ca8670bdc340c0faf48
PColares <siqueira.pedro1998@gmail.com>
2024-07-16 14:56:22 -0300

    16191/add: adicionando feature de guias em visualização de contrato

---
+++ src/pages/ContratosPage/VisualizarContrato/ContratoTabs.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="flex border-b">
+      <button
+        v-for="(tab, index) in tabs"
+        :key="index"
+        @click="selectedTab = tab"
+        :class="['py-2 px-4', { 'border-b-2 border-blue-500': selectedTab === tab }]"
+        class="focus:outline-none"
+      >
+        {{ tab }}
+      </button>
+    </div>
+
+    <div v-if="selectedTab === 'Contrato'" class="p-4">
+       <VisualizarContrato />
+    </div>
+
+    <div v-else-if="selectedTab.startsWith('Renovação')" class="p-4">
+      Renovação do contrato
+      <Renovacao :renovacao="selectedRenovacao" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, computed, watch } from 'vue';
+import { api } from '@/services/api';
+import { useRoute, useRouter,RouterLink } from 'vue-router';
+import { Icon } from "@iconify/vue";
+import VisualizarContrato from './VisualizarContrato.vue'
+import Renovacao from './RenovacaoContrato.vue';
+
+const router = useRouter();
+const route = useRoute();
+const contrato = ref({});
+const tabs = ref(['Contrato'])
+const selectedTab = ref('Contrato')
+const selectedRenovacao = ref(null);
+
+const fetchContrato = async (id) => {
+  try {
+    const response = await api.get(`/contratos/${id}`);
+    contrato.value = response.data;
+
+    if (contrato.value.renovacao && contrato.value.renovacao.length > 0) {
+      contrato.value.renovacao.forEach((_, index) => {
+        tabs.value.push(`Renovação ${index + 1}`);
+      });
+    }
+  } catch (error) {
+    console.error("Erro ao buscar contrato:", error);
+  }
+};
+
+onMounted(() => {
+  const contratoId = route.params.id;
+  fetchContrato(contratoId);
+});
+
+watch(selectedTab, (newTab) => {
+  if (newTab.startsWith('Renovação')) {
+    const renovacaoIndex = parseInt(newTab.split(' ')[1], 10) - 1;
+    selectedRenovacao.value = contrato.value.renovacao[renovacaoIndex];
+  } else {
+    selectedRenovacao.value = null;
+  }
+});
+
+</script>
---
+++ src/pages/ContratosPage/VisualizarContrato/RenovacaoContrato.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Renovação</h2>
+    <p><strong>Data de Início:</strong> {{ renovacao.dataInicio }}</p>
+    <p><strong>Data de Fim:</strong> {{ renovacao.dataFim }}</p>
+    <p><strong>Tipo:</strong> {{ renovacao.tipoRenovacao }}</p>
+    <p><strong>Porcentagem:</strong> {{ renovacao.porcentagemRenovacao }}%</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  renovacao: Object
+});
+
+</script>
